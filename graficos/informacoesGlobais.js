b8464219765ed8bef38f7faa61540ddee0b4f9fe
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-10-21 14:50:11 +0000

    teste

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,8 +3,18 @@ const endereco = "https://raw.githubusercontent.com/guilhermeonrails/api/refs/he
 async function visualizarInformacoesGlobais(){
     const resposta = await fetch(endereco)
     const dados = await resposta.json()
-    console.log(dados)
-    console.log("Total de pessoas no mundo: "+dados.total_pessoas_mundo);
+    //console.log(dados)
+
+    const paragrafo = document.createElement("p")
+    paragrafo.classList.add("graficos-container__texto")
+
+    paragrafo.innerHTML = `
+        Você sabia que o mundo tem ${dados.total_pessoas_mundo} bilhões de pessoas que aproximadamente ${dados.total_pessoas_conectadas} bilhões estão conectadas em alguma rede social e passam em média ${dados.tempo_medio}  horas conectadas.
+        Isso significa que aproximadamente 63.89% de pessoas conectadas em alguma rede social.
+    `
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+
 }
 
 visualizarInformacoesGlobais()
