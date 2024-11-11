1b8a3e32cbc36268e66588150ffd53a285cb2416
silviosnjr <silviosnjr@gmail.com>
2024-11-11 12:04:25 -0300

    Ajustes R7

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,12 +5,17 @@ async function visualizarInformacoesGlobais(){
     const dados = await resposta.json()
     //console.log(dados)
 
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 60)
+    const percentual = ((pessoasConectadas / pessoasNoMundo)*100).toFixed(2)
+
     const paragrafo = document.createElement("p")
     paragrafo.classList.add("graficos-container__texto")
 
     paragrafo.innerHTML = `
-        Você sabia que o mundo tem ${dados.total_pessoas_mundo} bilhões de pessoas que aproximadamente ${dados.total_pessoas_conectadas} bilhões estão conectadas em alguma rede social e passam em média ${dados.tempo_medio}  horas conectadas.
-        Isso significa que aproximadamente 63.89% de pessoas conectadas em alguma rede social.
+        Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas </span> e <span>${minutos} minutos </span>conectadas. <br>Isso significa que aproximadamente <span>${percentual} %</span> pessoas conectadas em alguma rede social.
     `
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
